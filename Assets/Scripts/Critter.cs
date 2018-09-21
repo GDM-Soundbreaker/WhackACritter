ce7ec33ed1e7965939a5942e80bc9cfd23e8123e
GDM-Soundbreaker <43067364+GDM-Soundbreaker@users.noreply.github.com>
2018-09-21 16:50:46 +0100

    added rotation and movement

--- Assets/Scripts/Critter.cs
+++ Assets/Scripts/Critter.cs
@@ -11,6 +11,7 @@ public class Critter : MonoBehaviour {
     public int pointValue = 1; //How much is this critter worth?
     public Timer timer;
 
+    private Vector2 direction; //What direction is the critter travelling in
 
 	// Use this for initialization
 	void Start () {
@@ -19,9 +20,21 @@ public class Critter : MonoBehaviour {
 	    transform.position = new Vector3(Random.Range(lowerRange.x, upperRange.x),
                 Random.Range(lowerRange.y, upperRange.y),
                 0);
+
+        //pick a random direction for this critter
+        direction.x = Random.Range(-1.0f, 1.0f);
+        direction.y = Random.Range(-1.0f, 1.0f);
+        direction = direction.normalized;
+
+        //Rotate the critter to face this direction#
+        transform.rotation = Quaternion.FromToRotation(Vector3.up, direction);
     }
+
     // Update is called once per frame
     void Update () {
+
+        transform.Translate(Vector2.up * Time.deltaTime);
+
 		if (timer.IsTimerRunning() == false)
         {
             Destroy(gameObject);
